5966849fa8812e5beb0be59d8102e19deec95f75
Raf Gemmail <rafiq@dreamthought.com>
2019-05-18 11:25:35 +0200

    Organized imports

--- src/test/java/com/openclassrooms/testing/CalculatorTest.java
+++ src/test/java/com/openclassrooms/testing/CalculatorTest.java
@@ -1,19 +1,48 @@
 package com.openclassrooms.testing;
 
-import org.junit.Test;
+import org.junit.*;
+
+import java.time.Duration;
+import java.time.Instant;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
-
 public class CalculatorTest {
+
+    private static Instant startTime;
+    private Calculator calculator;
+
+    @BeforeClass
+    public static void beforeClass() {
+        startTime = Instant.now();
+        System.out.println("CALLED ONLY ONCE BEFORE ALL TESTS AT " + startTime);
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        Instant endedAt = Instant.now();
+        Duration duration = Duration.between(startTime, endedAt);
+        System.out.println("CALLED AFTER ALL TESTS - ONLY ONCE - AT " + duration);
+    }
+
+    @Before
+    public void setUp() {
+        System.out.println("CALLED BEFORE A TEST @Before");
+        calculator = new Calculator();
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("CALLED AFTER A TEST @After");
+        calculator = null;
+    }
 
     @Test
     public void add_shouldReturnTheSum_ofTwoNumbers() {
         // arrange
         Integer expected = 3; // 1+2
-        Calculator calculator = new Calculator();
 
         // act
         Integer result = calculator.add(1, 2);
@@ -25,13 +54,12 @@ public class CalculatorTest {
     @Test
     public void multiply_shouldReturnTheProduct_ofTwoNumbers() {
         // arrange
-        Calculator calculator = new Calculator();
+        Integer expected = 6;
 
         // act
         Integer product = calculator.multiply(2, 3);
 
         // assert that product is equal to 6
-        Integer expected = 6;
         assertThat(product, is( equalTo(expected))); // 2x3
     }
 
