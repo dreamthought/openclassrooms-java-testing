7a05e3ca34c66c4010f3ae9ddeae6bf1d57c9cdb
Raf Gemmail <rafiq@dreamthought.com>
2019-06-14 11:13:52 +1200

    Added autowired to the constructor

--- src/main/java/com/openclassrooms/testing/calculator/service/CalculatorService.java
+++ src/main/java/com/openclassrooms/testing/calculator/service/CalculatorService.java
@@ -3,6 +3,7 @@ package com.openclassrooms.testing.calculator.service;
 import com.openclassrooms.testing.calculator.domain.Calculator;
 import com.openclassrooms.testing.calculator.domain.SolutionFormatter;
 import com.openclassrooms.testing.calculator.domain.model.CalculationModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,6 +11,7 @@ public class CalculatorService implements Solver {
     Calculator calculator;
     private SolutionFormatter formatter;
 
+    @Autowired
     public CalculatorService(Calculator calculator, SolutionFormatter formatter) {
         this.calculator = calculator;
         this.formatter = formatter;
